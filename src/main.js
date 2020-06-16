b5271296826aa7ca6e8d6d66f1418f1be0da148b
喵 <2583081951@qq.com>
2020-06-16 12:59:46 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// 入口文件
+import Vue from 'vue'
+
+//导入Mui
+import './lib/mui/css/mui.css'
+
+//按需导入Mint-UI中的组件
+import { Header } from 'mint-ui'
+Vue.component(Header.name,Header)
+
+//导入App组件
+import app from './App.vue'
+
+
+
+var vm=new Vue({
+	el:'#app',
+	render:c => c(app)
+	
+})
